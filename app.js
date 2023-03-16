9cfbd584b87e1cc8a4f7f12e6eda8d72f5431197
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-03-16 16:43:59 -0500

    added Javascript file

---
+++ app.js
@@ -0,0 +1,33 @@
+let answerOne = 'y';
+let answerTwo = 'n';
+let theirName = "";
+
+let interestBlackHoles = prompt("Hello space traveler! Are you interested in the awesomeness of black holes? (type 'y' for yes or 'n' for no)");
+
+if (interestBlackHoles == answerOne){
+
+  theirName = prompt("What's your name traveler!");
+  document.write ("Welcome " + theirName + "! Below is some information for you to explore the wonders of black holes!");
+
+} else if (interestBlackHoles == answerTwo) {
+  alert("Sorry to hear that! Feel free to check out this website for info on black holes. You may find it interesting!");
+
+} else {
+  alert("Please enter a valid response ('y' for yes or 'n' for no");
+}
+
+function changeBackgroundColor() {
+    let text = "Press Ok to change background color! Or press cancel.";
+    let color = "#000000";
+    if (confirm(text) == true) {
+        text = "Black color has been sucked away!";
+        color = "#770000";   
+    } else {
+        text = "You have chosen to stay with black!";
+        color ="#000000";
+    }
+    document.getElementById("decision-of-colors").innerHTML = text;
+    document.body.style.background = color;
+    
+ }
+
